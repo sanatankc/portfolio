8e742cdc770bd455a282bc0e83b9d1e2d829253e
sanatankc <sanatan.iit@gmail.com>
2025-08-18 17:12:25 +0530

    reduce navigator values

--- app/components/MobileNav.tsx
+++ app/components/MobileNav.tsx
@@ -14,7 +14,7 @@ const MobileNav: React.FC = () => {
   return (
     <div className="fixed bottom-0 left-0 right-0 h-[calc(60px+env(safe-area-inset-bottom))] pb-[env(safe-area-inset-bottom)] bg-black/70 flex justify-around items-center z-[100000]">
       <button className="p-2 text-white" onClick={() => {
-        navigator.vibrate(200);
+        navigator.vibrate(50);
         if (areAllWindowsHidden) {
           showAllWindows();
         } else {
@@ -24,7 +24,7 @@ const MobileNav: React.FC = () => {
         {areAllWindowsHidden ? <i className="hn hn-grid text-3xl"></i> : <i className="hn hn-home text-3xl"></i> }
       </button>
       <button className="p-2 text-white" onClick={() => {
-        navigator.vibrate(200);
+        navigator.vibrate(50);
         toggleAppSwitcher();
       }}>
         <OverviewIcon />
